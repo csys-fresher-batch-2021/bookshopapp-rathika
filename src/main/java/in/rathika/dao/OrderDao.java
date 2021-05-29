fc4e29bc1d91723d7e1962889ecdf1312d61c994
Rathika <rathikaalagar12@gmail.com>
2021-05-29 17:09:58 -0500

    feature3 code smells removed

--- src/main/java/in/rathika/dao/OrderDao.java
+++ src/main/java/in/rathika/dao/OrderDao.java
@@ -8,27 +8,30 @@ import in.rathika.model.Order;
 
 public class OrderDao {
 	/**
-	 *Store Ordered Book Details
+	 * Store Ordered Book Details
 	 */
 	private static final List<Order> orders = new ArrayList<>();
+
 	/**
 	 * Add orderd Books into arraylist.
+	 * 
 	 * @param bookName
 	 * @param language
 	 * @param noOfBooks
 	 * @param cost
 	 */
-	public void addCart(String bookName, String language, int noOfBooks,double cost) {
-		
+	public void addCart(String bookName, String language, int noOfBooks, double cost) {
+
 		orders.add(new Order(bookName, language, noOfBooks, cost));
 	}
+
 	/**
 	 * Get Ordered List.
+	 * 
 	 * @return
 	 */
-    public static List<Order> getOrder(){
-   	 return orders;
-    }
-    
-    
+	public static List<Order> getOrder() {
+		return orders;
+	}
+
 }
--- src/main/java/in/rathika/servlet/DeleteBookServlet.java
+++ src/main/java/in/rathika/servlet/DeleteBookServlet.java
@@ -17,7 +17,7 @@ import in.rathika.service.OrderService;
 @WebServlet("/DeleteBookServlet")
 public class DeleteBookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+    @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
