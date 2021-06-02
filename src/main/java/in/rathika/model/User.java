4f229096026853bbb260b4f08879d584db3f6e1a
Rathika <rathikaalagar12@gmail.com>
2021-06-01 21:10:14 -0500

    adding modified files

--- src/main/java/in/rathika/model/User.java
+++ src/main/java/in/rathika/model/User.java
@@ -9,56 +9,36 @@ public class User {
     private int age;
     private String password;
     private String confrimPassword;
-	public String getPassword() {
-		return password;
-	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	public String getConfrimPassword() {
 		return confrimPassword;
 	}
-	public void setConfrimPassword(String confrimPassword) {
-		this.confrimPassword = confrimPassword;
-	}
+	
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	
 	public String getEmail() {
 		return email;
 	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
 	public Long getMobile() {
 		return mobile;
 	}
-	public void setMobile(Long mobile) {
-		this.mobile = mobile;
-	}
+	
 	public String getAddress() {
 		return address;
 	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
+	
 	public int getAge() {
 		return age;
 	}
-	public void setAge(int age) {
-		this.age = age;
-	}
 
-    
-	
-	
 
-	
-	
-	@Override
+   	@Override
 	public String toString() {
 		return "User [name=" + name + ", email=" + email + ", mobile=" + mobile + ", address=" + address + ", age="
 				+ age + ", password=" + password + "]";
