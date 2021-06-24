9d1ce4912b0ac96d2106fc516eb489c9f3ae9655
Rathika12 <rathikaalagar12@gmail.com>
2021-06-24 16:10:52 +0530

    fix bugs

--- src/main/java/in/rathika/servlet/SearchServlet.java
+++ src/main/java/in/rathika/servlet/SearchServlet.java
@@ -14,7 +14,7 @@ import in.rathika.service.BookService;
 @WebServlet("/SearchServlet")
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
+    @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	     try {
 	    	 String name = request.getParameter("search");
